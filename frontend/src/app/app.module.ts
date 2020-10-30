3d37aa0b829e3dc373ab58a5f26b3306f22be13b
Sebastian Cordero Ramirez <scordero414@gmail.com>
2020-10-30 18:03:20 -0500

    Creacion de componentes signup y pag-principal

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PaginaPrincipalComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
