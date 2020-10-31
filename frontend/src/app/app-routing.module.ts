2d2832afe736a52d9ef82824ee5159d2e10d9f88
Michael Steven <stevenpnd@gmail.com>
2020-10-30 21:55:59 -0500

    Se creó la interfaz de register

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaginaPrincipalComponent } from './pagina-principal/pagina-principal.component';
 import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [ {path: 'register', component: RegisterComponent}];
+const routes: Routes = [
+  { path: '', component: PaginaPrincipalComponent },
+  { path: 'register', component: RegisterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
